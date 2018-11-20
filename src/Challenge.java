65479958aaab93ee999ae5742a2ef924fb2155fb
VaishakVellore <vaishak.vellore@gmail.com>
2018-11-20 00:10:37 -0700

    Automating jsoup website connection

--- src/Challenge.java
+++ src/Challenge.java
@@ -6,10 +6,9 @@ import org.jsoup.nodes.Element;
 public class Challenge {
 
     public static void main(String[] args) {
-        String url_open ="http://hpa.services/automation-challenge/";
+        String blogUrl = "http://hpa.services/automation-challenge/";
         try {
-            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
-            Document doc = Jsoup.connect("http://hpa.services/automation-challenge/").get();
+            Document doc = Jsoup.connect(blogUrl).get();
             doc.select("p").forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
