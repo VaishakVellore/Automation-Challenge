a06967b31fd73795cffd0a4948288b55e64f88de
VaishakVellore <vaishak.vellore@gmail.com>
2018-11-20 00:02:48 -0700

    Adding Jsoup libraries to interpret HTML Page

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Challenge {
 
@@ -6,8 +9,11 @@ public class Challenge {
         String url_open ="http://hpa.services/automation-challenge/";
         try {
             java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
+            Document doc = Jsoup.connect("http://hpa.services/automation-challenge/").get();
+            doc.select("p").forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
